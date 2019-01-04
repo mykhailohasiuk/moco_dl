5bb3ebd8b435d55c92cd9cc757a6d4cdacdaf0f4
Mykhailo Hasiuk <mhas0023@dyn-49-127-20-238.its.monash.edu.au>
2019-01-04 18:07:28 +1100

    friday inght commit

--- spherical_transform/spherical_coordinates.py
+++ spherical_transform/spherical_coordinates.py
@@ -1,4 +1,5 @@
 import numpy as np
+import math
 
 def cart2sph(x, y, z):
     hxy = np.hypot(x, y)
@@ -7,6 +8,7 @@ def cart2sph(x, y, z):
     az = np.arctan2(y, x)
     return az, el, r
 
+
 def sph2cart(az, el, r):
     rcos_theta = r * np.cos(el)
     x = rcos_theta * np.cos(az)
@@ -14,17 +16,42 @@ def sph2cart(az, el, r):
     z = r * np.sin(el)
     return x, y, z
 
-def to_spherical_from_imge_center(image):
-    image_center_x, image_center_y, image_center_z = image.shape[0]/2, image.shape[1]/2, image.shape[2]/2
 
-    spherical_image = np.zeros(image.shape)
+def appendSpherical_np(xyz):
+    ptsnew = np.array(np.zeros(xyz.shape))
+    xy = xyz[:,0]**2 + xyz[:,1]**2
+    ptsnew[:,0] = np.sqrt(xy + xyz[:,2]**2)
+    ptsnew[:,1] = np.arctan2(np.sqrt(xy), xyz[:,2]) # for elevation angle defined from Z-axis down
+    #ptsnew[:,1] = np.arctan2(xyz[:,2], np.sqrt(xy)) # for elevation angle defined from XY-plane up
+    ptsnew[:,2] = np.arctan2(xyz[:,1], xyz[:,0])
+    return ptsnew
 
-    for x in range(image.shape[0]):
-        for y in range(image.shape[1]):
-            for z in range(image.shape[2]):
-                point_x = x - image_center_x
-                point_y = y - image_center_y
-                point_z = z - image_center_z
 
-                r, el, az = cart2sph(point_x, point_y, point_z)
+def find_spherical_matrix_dimensions(cartesian_image_shape):
+    theta = 360
+    phi = 360
+    hxy = np.hypot(cartesian_image_shape[0], cartesian_image_shape[1])
+    r = int(math.ceil(np.hypot(hxy, cartesian_image_shape[2])))
+
+    return (theta, phi, r)
+
+
+def create_spherical_matrix_from_cartesian(cartesian_image):
+
+    spherical_image = np.zeros((find_spherical_matrix_dimensions(cartesian_image.shape)))
+    print('spherical matrix shape: ', spherical_image.shape)
+
+    for az in range(spherical_image.shape[0]):
+        for el in range(spherical_image.shape[1]):
+            for r in range(spherical_image.shape[2]):
+                x, y, z = int(math.floor(sph2cart(az, el, r)[0])), int(math.floor(sph2cart(az, el, r)[1])), int(math.floor(sph2cart(az, el, r)[2]))
+                print('-----looking for coordiantes: ', x, y, z, '\n')
+                # spherical_image[r, el, az] = cartesian_image[x, y, z]
+        print(r)
+    return spherical_image
+
+
+testImage = create_spherical_matrix_from_cartesian(np.zeros((64, 64, 64)))
+print(testImage.shape)
+
 
